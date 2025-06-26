b07c44da77e07c78968e1c4dfa5949f1451afc64
khaya-24 <khayahealthtech@gmail.com>
2025-06-26 13:26:34 +0200

    handles upload of multiple files using .zip method

--- khaya_copy.py
+++ khaya_copy.py
@@ -1,5 +1,7 @@
+# Script for Khayahealthtech to auto-copy files from SD-card to gcloud storage
+# Written by: Qhamani Maqungu
+# Date Modified: 26 June 2025
 
-import subprocess # handles subprocesses like crating venv
 
 import os
 import shutil
@@ -10,6 +12,7 @@ import time
 from datetime import date
 
 import venv
+import zipfile
 
 # to run gcloud 
 from google.cloud import storage
@@ -19,7 +22,7 @@ import RPi.GPIO as GPIO
 
 venv_dir = "/home/khayahealthtech/khaya_ws/venv"
 DEST_FOLDER = "./khaya_ws/Patient_Data"
-patient_data = "./khaya_ws/Patient_Data/KHAYA1/khaya_usb_copy.py" 
+patient_data = "./khaya_ws/Patient_Data.zip" 
 
 ip_address = "8.8.8.8"  # Google's DNS
 
@@ -27,7 +30,7 @@ bucket_name = "khaya-app-1.firebasestorage.app"
 device_name = "device-001"
 today = date.today().strftime('%Y%m%d')
 
-#find the mount poinyt for USB insertion
+#find the mount point for USB insertion
 mount_point = "/media/khayahealthtech/KHAYA"
 
 
@@ -58,6 +61,21 @@ time.sleep(3)
 def main():
 	monitor_usb()
 
+
+
+# Creating a zip-folder for the patient data
+def createZipFolder(source, zip_folder):
+
+	try:
+		with zipfile.ZipFile(zip_folder, 'w', zipfile.ZIP_DEFLATED) as zipf:
+			for root, _, files in os.walk(source):
+				for file in files:
+					full_path = os.path.join(root, file)
+					relative_path = os.path.relpath(full_path, source)
+					zipf.write(full_path, relative_path)
+	except Exception as e:
+		print("failed to compress file")]
+
 def statusIndicator(mode):
 
 	match mode:
@@ -78,7 +96,7 @@ def statusIndicator(mode):
 # Locate the KHAYA device once mounted
 def get_mounted_usb():
 	for partition in psutil.disk_partitions(all=True):
-		if "/media/khayahealthtech/KHAYA" in partition.mountpoint: # Adjust for different OS paths if needed
+		if "/media/khayahealthtech/KHAYA" in partition.mountpoint: 
 			mountpoint = partition.mountpoint
 			return partition.mountpoint
 	return None
@@ -89,7 +107,7 @@ def get_mounted_usb():
 # using the mountpoint and the destination folder "Patient_Data"
 def copy_files(src, dst):
 	if not os.path.exists(dst):
-		os.makedirs(dst) #creating new folder if one doesn't exist
+		os.makedirs(dst) # creating new folder if one doesn't exist
 	for root, _, files in os.walk(src):
 		for file in files:
 			src_path = os.path.join(root, file)
@@ -114,13 +132,12 @@ def monitor_usb():
 		backup_folder = os.path.join(DEST_FOLDER, os.path.basename(mount_point))
 		copy_files(mount_point, backup_folder)
 
-
 		print("done copying locally")
 		GPIO.output(localCopy_LED, GPIO.LOW)
 
 		upload_to_cloud(bucket_name, patient_data, patient_name);
 
-   	#Then listens for USB insertions and copies data locally.
+   	# Then listens for USB insertions and copies data locally.
 	context = pyudev.Context()
 	monitor = pyudev.Monitor.from_netlink(context)
 	monitor.filter_by(subsystem="block")
@@ -141,57 +158,60 @@ def monitor_usb():
 
 				try:
 					copy_files(usb_path, backup_folder)
+
+					# Zip the locally copied folder before uploading to gcloud storage
+					createZipFolder(DEST_FOLDER,patient_data)
+
+					# Making sure the file is compressed before attempting to uplad
+					time.sleep(2)
 					print("Stage 1 upload complete!")
+
 				except Exception as e :
 					print("failed to copy locally: ", e)
 
 
-				#Init uploading to gcloud
+				# Init uploading to gcloud, check internet connection using  google's DSN server
 				if(isDeviceOnline):
 					statusIndicator("online")
 					patient_id = device_name + today
 					upload_to_cloud(bucket_name, patient_data, patient_id);
 
 					statusIndicator("stand-by")
+
+				else:
+					print("please make sure device is connected to the internet")
 def isDeviceOnline():
 
-#check internet connection before coppying files to the internet
+# check internet connection before coppying files to the internet
 	try:
 		subprocess.run(["ping", "-c", "1", "-w", "2", ip_address], timeout=2, check=True)
-		print("Connected to the internet")
 		return True
 	except subprocess.CalledProcessError:
-		print("No internet connection")
 		return False
 
 def upload_to_cloud(bucket_name, source, destination_blob_name):
-    #Uploads a file to the bucket
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
+	#Uploads a file to the bucket
+	# The ID of your GCS bucket
+	# bucket_name = "your-bucket-name"
+	# The path to your file to upload
+	# source_file_name = "local/path/to/file"
+	# The ID of your GCS object
+	# destination_blob_name = "storage-object-name"
 
 	storage_client = storage.Client()
 	bucket = storage_client.bucket(bucket_name)
 	blob = bucket.blob(destination_blob_name)
 
-    # Optional: set a generation-match precondition to avoid potential race conditions
-    # and data corruptions. The request to upload is aborted if the object's
-    # generation number does not match your precondition. For a destination
-    # object that does not yet exist, set the if_generation_match precondition to 0.
-    # If the destination object already exists in your bucket, set instead a
-    # generation-match precondition using its generation number.
 	generation_match_precondition = 1
 
 	blob.upload_from_filename(source)
-	#blob.upload_from_filename(source, if_generation_match=generation_match_precondition)
+	# blob.upload_from_filename(source, if_generation_match=generation_match_precondition)
 
 	print(f"File {source} uploaded to {destination_blob_name}.")
 
 	statusIndicator("stand-by")
 	exit()
+	GPIO.cleanup()
 
 
 if __name__ == "__main__":
